07e04171f8068a913c76ffa9c371154163e04eff
akaur121 <akaur162@flemingcollege.ca>
2023-05-25 00:02:30 -0700

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #This is a good programming advice
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("If the implementation is hard to explain,its a bad idea.")
